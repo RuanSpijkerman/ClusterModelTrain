32e2b791edf2b8f62495b2ff4332de93f3d53284
RuanSpijkerman <ruan.spijkerman@hotmail.com>
2020-09-26 22:34:15 +0200

    Add project files.

---
+++ ClusterModelTrain/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClusterModelTrain
+{
+    class Program
+    {
+        static readonly string _dataPath1 = "D:\\Universiteit\\Discovery\\Training\\ClusterModelTrain\\ClusterModelTrain\\Data\\Group5.csv";
+
+
+        static readonly string _modelPath = "D:\\Universiteit\\Discovery\\Training\\ClusterModelTrain\\ClusterModelTrain\\Data\\IncomeCluster5.zip";
+
+        public static void TrainOption2()
+        {
+
+            var mlContext = new MLContext(seed: 0);
+            IDataView trainingData = mlContext.Data.LoadFromTextFile<UserData>(_dataPath1, hasHeader: true, separatorChar: ',');
+
+            var options = new KMeansTrainer.Options
+            {
+                NumberOfClusters = 4,
+                NumberOfThreads = 1,
+                FeatureColumnName = "Utilities"
+            };
+
+            var pipeline = mlContext.Clustering.Trainers.KMeans(options);
+
+
+            var model = pipeline.Fit(trainingData);
+
+            VBuffer<float>[] centroids = default;
+
+            var modelParams = model.Model;
+            modelParams.GetClusterCentroids(ref centroids, out int k);
+            Console.WriteLine(
+                $"The first 3 coordinates of the first centroid are: " +
+                string.Join(", ", centroids[0].GetValues().ToArray().Take(3)));
+
+
+        }
+     
+
+
+
+        public void TrainOption1() {
+            var mlContext = new MLContext(seed: 0);
+
+            IDataView dataView = mlContext.Data.LoadFromTextFile<UserData>(_dataPath1, hasHeader: false, separatorChar: ',');
+
+            
+            string featuresColumnName = "Features";
+
+
+            var pipeline = mlContext.Transforms
+                .Concatenate(featuresColumnName, "Utilities", "Transport", "Rent", "Loans", "Saving", "Insurance", "Education", "Communication", "Takeout", "Groceries", "Alcohol", "Entertainment", "Personal", "Clothing", "Cash", "Other")
+                .Append(mlContext.Clustering.Trainers.KMeans(featuresColumnName, numberOfClusters: 4));
+
+            var model = pipeline.Fit(dataView);
+
+            /*
+            using (var fileStream = new FileStream(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                mlContext.Model.Save(model, dataView.Schema, fileStream);
+            }
+            */
+            var prediction = predictor.Predict(new UserData());
+        }
+
+        static void Main(string[] args)
+        {
+
+           TrainOpt
+
+
+        }
+            
+        }
+}
---
+++ ClusterModelTrain/UserData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace ClusterModelTrain
+{
+    public class UserData
+    {
+        [LoadColumn(0)]
+        public float Utilities;
+        [LoadColumn(1)]
+        public float Transport;
+        [LoadColumn(2)]
+        public float Rent;
+        [LoadColumn(3)]
+        public float Loans;
+        [LoadColumn(4)]
+        public float Saving;
+        [LoadColumn(5)]
+        public float Insurance;
+        [LoadColumn(6)]
+        public float Education;
+        [LoadColumn(7)]
+        public float Communication;
+        [LoadColumn(8)]
+        public float Takeout;
+        [LoadColumn(9)]
+        public float Groceries;
+        [LoadColumn(10)]
+        public float Alcohol;
+        [LoadColumn(11)]
+        public float Entertainment;
+        [LoadColumn(12)]
+        public float Personal;
+        [LoadColumn(13)]
+        public float Clothing;
+        [LoadColumn(14)]
+        public float Cash;
+        [LoadColumn(15)]
+        public float Other;
+    }
+
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+}
